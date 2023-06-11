c14ef302d70357ba16e63a5c20f22fbd4ca88b81
Baba-Yaga-07 <115881621+Baba-Yaga-07@users.noreply.github.com>
2023-06-11 22:15:56 +0530

    0

--- file_rev_para.c
+++ file_rev_para.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 int main() {
     FILE *file;
     char ch,line[1000];
-    file = fopen("input.txt", "w");
+    file = fopen("input.txt", "w+");
     printf("Enter a paragraph :\n");
     while((ch=getchar())!=EOF)
         fputc(ch,file);
-    fclose(file);
-    file = fopen("input.txt", "r");
+    rewind(file);
     while (fgets(line, sizeof(line), file)) 
     {
         char *token,c;
         token = strtok(line, " \n");
         while (token != NULL) 
         {
-            int l = strlen(token);
-            for (int i = 0; i <= l/2; i++) 
-                {
-                    c=token[i];
-                    token[i]=token[l-i-1];
-                    token[l-i-1]=c;
-                }
+            strrev(token);
             printf("%s ", token);
             token = strtok(NULL, " \n");
         }
